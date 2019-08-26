f9002eab74bc6fc65d8d9fb3555e377c241ced3c
John Chen <johnchen456@gmail.com>
2019-08-26 12:26:01 -0400

    test: version output is stderr

--- molecule/default/tests/test_default.rb
+++ molecule/default/tests/test_default.rb
@@ -3,6 +3,6 @@
 describe command('saslauthd -v') do
   # rubocop:disable LineLength
 
-  its('stdout') { should eq "saslauthd 2.1.27\nauthentication mechanisms: sasldb getpwent kerberos5 pam rimap shadow ldap\n\n" }
+  its('stderr') { should eq "saslauthd 2.1.27\nauthentication mechanisms: sasldb getpwent kerberos5 pam rimap shadow ldap\n\n" }
   # rubocop:enable LineLength
 end
