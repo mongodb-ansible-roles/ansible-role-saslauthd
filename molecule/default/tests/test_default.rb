694d814b19c6ca410eeacd3081582c6008ef391a
John Chen <johnchen456@gmail.com>
2019-08-26 11:21:10 -0400

    feat: new role for saslauthd

---
+++ molecule/default/tests/test_default.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+describe command('saslauthd -v') do
+  # rubocop:disable LineLength
+
+  its('stdout') { should eq "saslauthd 2.1.27\nauthentication mechanisms: sasldb getpwent kerberos5 pam rimap shadow ldap\n\n" }
+  # rubocop:enable LineLength
+end
